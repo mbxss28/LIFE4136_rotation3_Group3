93ee55db6e8d8fc679e2e93f85ac1530d0263fba
mbxss28 <swanesamuel@gmail.com>
2025-04-14 15:11:59 +0100

    Update gene_ann.sh

--- GENE_Annotatition/gene_ann.sh
+++ GENE_Annotatition/gene_ann.sh
@@ -16,13 +16,13 @@ source $HOME/.bash_profile
 
 conda activate Augustus
 
-#Dip1=/share/BioinfMSc/rot3_group3/C087_203_mapq_hap1_8.fa
-#Dip2=/share/BioinfMSc/rot3_group3/C087_203_mapq_hap2_8.fa
+Dip1=/share/BioinfMSc/rot3_group3/C087_203_mapq_hap1_8.fa
+Dip2=/share/BioinfMSc/rot3_group3/C087_203_mapq_hap2_8.fa
 
-#augustus --gff3=on --species=arabidopsis $Dip1 > /share/BioinfMSc/rot3_group3/gene_annotation/Dip1.gff
-#augustus --gff3=on --species=arabidopsis $Dip2 > /share/BioinfMSc/rot3_group3/gene_annotation/Dip2.gff
+augustus --gff3=on --species=arabidopsis $Dip1 > /share/BioinfMSc/rot3_group3/gene_annotation/Dip1.gff
+augustus --gff3=on --species=arabidopsis $Dip2 > /share/BioinfMSc/rot3_group3/gene_annotation/Dip2.gff
 
-#augustus --species=arabidopsis hap1_region.fasta > /share/BioinfMSc/rot3_group3/gene_annotation/hap1region.gff
+ugustus --species=arabidopsis hap1_region.fasta > /share/BioinfMSc/rot3_group3/gene_annotation/hap1region.gff
 augustus --species=arabidopsis hap2_region.fasta > /share/BioinfMSc/rot3_group3/gene_annotation/hap2region.gff
 
 conda deactivate
